87f95fd066c3eb29bc29d95fd4ea4e2ce98c613d
Gabriele Rulli <rulligabri.95@gmail.com>
2016-11-15 16:08:31 +0100

    Mannaggia al current user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
 
   def index
     @latest_room = Room.latest
-    @preferred_rooms = current_user.preferred_rooms.map(&:room)
+    if current_user
+      @preferred_rooms = current_user.preferred_rooms.map(&:room)
+    end
   end
 end
