c0af29228613872d082384efb6d267376b4b8836
Gabriele Rulli <rulligabri.95@gmail.com>
2016-11-15 16:06:31 +0100

    Skip authenticate user home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
     @latest_room = Room.latest
     @preferred_rooms = current_user.preferred_rooms.map(&:room)
