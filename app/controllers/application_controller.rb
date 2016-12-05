9e066cf6bfe8864ca3df58569a5cdfe7ae3b2efb
Andrea Macchieraldo <a.macchieraldo@gmail.com>
2016-12-05 19:36:12 +0100

    Minor Fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,15 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def check_for_mobile
+    session[:mobile_override] = params[:mobile] if params[:mobile]
+    send_to_mobile if mobile_device?
+  end
+
+  def mobile_device? 
+    (request.user_agent =~ /Mobile|webOS/) && (request.user_agent !~ /iPad/) || session[:mobile_override] == "true" # Season this regexp to taste. I prefer to treat iPad as non-mobile.
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname, :birth_date, :nickname, :address, :comune, :prov, :zip_code])
   end
