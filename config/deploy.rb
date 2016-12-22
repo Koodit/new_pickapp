60634a1dea6283a07e54e1e2bb1656b473216f6b
Andrea Macchieraldo <a.macchieraldo@gmail.com>
2016-12-22 11:39:34 +0100

    Capi Setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,59 +1,51 @@
-# config valid only for current version of Capistrano
-# lock '3.4.0'
+# Change these
+
 require 'capistrano-db-tasks'
 
-set :application, 'pick'
-set :repo_url, 'git@github.com:Koodit/pickapp_rails.git'
+set :repo_url,        'git@github.com:Koodit/new_pickapp.git'
+set :application,     'pickapp'
+set :user,            'koodit'
 
-set :rvm_ruby_version, '2.2.1'
+set :rvm_ruby_version, '2.3.1'
 
-# set :passenger_rvm_ruby_version, '2.2.1'
-
-# set :passenger_restart_with_sudo, true
-set :passenger_restart_with_touch, true
-
+# Don't change these unless you know what you're doing
 set :pty, false
 set :use_sudo, false
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
 # set :keep_releases, 5
 
-# namespace :deploy do
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/db.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
+namespace :deploy do
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      invoke 'deploy'
+      puts 'Adding nginx.conf to sites-available in /etc/nginx/sites-enabled'
+      execute "ln -nfs /home/#{fetch(:user)}/apps/#{fetch(:application)}/current/config/nginx.conf /etc/nginx/sites-enabled/#{fetch(:application)}"
+    end
+  end
 
-# end
+  # after :restart, :clear_cache do
+  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #     execute "cd #{deploy_to}/current && ( RAILS_ENV=production ~/.rvm/bin/rvm 2.2.2 do bundle exec rake searchkick:reindex CLASS=Event)"
+  #     execute "cd #{deploy_to}/current && ( RAILS_ENV=production ~/.rvm/bin/rvm 2.2.2 do bundle exec rake searchkick:reindex CLASS=Attraction)"
+  #     execute "cd #{deploy_to}/current && ( RAILS_ENV=production ~/.rvm/bin/rvm 2.2.2 do bundle exec rake searchkick:reindex CLASS=AttractionCategory)"
+  #     execute "cd #{deploy_to}/current && ( RAILS_ENV=production ~/.rvm/bin/rvm 2.2.2 do bundle exec rake searchkick:reindex CLASS=SportCategory)"
+  #     execute "cd #{deploy_to}/current && ( RAILS_ENV=production ~/.rvm/bin/rvm 2.2.2 do bundle exec rake searchkick:reindex CLASS=SportEntity)"
+  #     execute "cd #{deploy_to}/current && ( RAILS_ENV=production ~/.rvm/bin/rvm 2.2.2 do bundle exec rake searchkick:reindex CLASS=ArtCategory)"
+  #     execute "cd #{deploy_to}/current && ( RAILS_ENV=production ~/.rvm/bin/rvm 2.2.2 do bundle exec rake searchkick:reindex CLASS=ArtEntity)"
+  #     execute "cd #{deploy_to}/current && ( RAILS_ENV=production ~/.rvm/bin/rvm 2.2.2 do bundle exec rake searchkick:reindex CLASS=OutdoorCategory)"
+  #     execute "cd #{deploy_to}/current && ( RAILS_ENV=production ~/.rvm/bin/rvm 2.2.2 do bundle exec rake searchkick:reindex CLASS=OutdoorEntity)"
+  #   end
+  # end
+end
