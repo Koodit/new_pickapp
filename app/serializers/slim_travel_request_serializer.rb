cc7c609a5236fb565e8b3fe70cde8cb720693712
Andrea Macchieraldo <a.macchieraldo@gmail.com>
2017-03-29 15:07:08 +0200

    Fix slim serializers for travel and requests

--- app/serializers/slim_travel_request_serializer.rb
+++ app/serializers/slim_travel_request_serializer.rb
@@ -1,11 +1,12 @@
 class SlimTravelRequestSerializer < ActiveModel::Serializer
-  attributes :id, :back_datetime, :is_one_way, :lat, :lng, :one_way_datetime, :starting_address, :is_owner, :can_repay, :towards_room, :only_with_feedback, :flexible_departure
+  attributes :id, :back_datetime, :is_one_way, :lat, :lng, :one_way_datetime, :starting_address, :is_owner, :can_repay, :towards_room, :only_with_feedback, :flexible_departure,
+  :user
 
   has_one :user, serializer: SlimTravelDriverSerializer
   has_one :room, serializer: SlimRoomSerializer
   
   def user
-    object.passenger
+    SlimUserSerializer.new(object.driver, scope: scope, root: false)
   end
 
   def is_owner
--- app/serializers/slim_travel_serializer.rb
+++ app/serializers/slim_travel_serializer.rb
@@ -2,9 +2,8 @@ class SlimTravelSerializer < ActiveModel::Serializer
 
   attributes :id, :is_owner,
   :departure_datetime, :destination_address, :destination_lat, :destination_lng,
-  :starting_address, :starting_lat, :starting_lng, :towards_room, :flexible_departure, :only_with_feedback, :can_repay
-
-  has_one :user, serializer: SlimTravelDriverSerializer
+  :starting_address, :starting_lat, :starting_lng, :towards_room, :flexible_departure, :only_with_feedback, :can_repay,
+  :user
 
   has_many :travel_stops
 
@@ -13,10 +12,9 @@ class SlimTravelSerializer < ActiveModel::Serializer
   def room
     object.room
   end
-  
 
   def user
-    object.driver
+    SlimUserSerializer.new(object.driver, scope: scope, root: false)
   end
 
   def is_owner
