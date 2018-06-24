51fecde5fb4ffb3a9715025c40957322141f9a9c
Roberto Vergallo <>
2018-06-24 21:00:55 +0200

    fixes

--- app/mailers/pick_app_mailer.rb
+++ app/mailers/pick_app_mailer.rb
@@ -1,3 +1,5 @@
+require 'one_signal'
+
 class PickAppMailer < ApplicationMailer
   def send_email(to, subject, body, link, player_id)
 
@@ -6,16 +8,17 @@ class PickAppMailer < ApplicationMailer
          body: body+"<br/><br/>"+link,
          content_type: 'text/html')
 
-	params = {
-	  app_id: 'bf81fd5f-ada4-41ef-bd01-b44ef4cafd45',
-	  headings: subject,
-	  contents: body,
-	  ios_badgeType: 'None',
-	  ios_badgeCount: 1,
-	  include_player_ids: [player_id]
-	}
-	response = OneSignal::Notification.create(params: params)
-	notification_id = JSON.parse(response.body)["id"]
+    params = {
+      app_id: 'bf81fd5f-ada4-41ef-bd01-b44ef4cafd45',
+      headings: subject,
+      contents: body,
+      ios_badgeType: 'None',
+      ios_badgeCount: 1,
+      include_player_ids: [player_id]
+    }
+   
+    response = OneSignal::Notification.create(params: params)
+    notification_id = JSON.parse(response.body)["id"]
 
   end
 end
