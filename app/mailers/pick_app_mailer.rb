4a6d797f33ee5a96c5744606b98ed0e899601d57
Roberto Vergallo <>
2018-06-24 20:34:06 +0200

    test

--- app/mailers/pick_app_mailer.rb
+++ app/mailers/pick_app_mailer.rb
@@ -1,29 +1,21 @@
 class PickAppMailer < ApplicationMailer
   def send_email(to, subject, body, link, player_id)
+
+	params = {
+	  app_id: 'bf81fd5f-ada4-41ef-bd01-b44ef4cafd45',
+	  headings: subject,
+	  contents: body,
+	  ios_badgeType: 'None',
+	  ios_badgeCount: 1,
+	  include_player_ids: [player_id]
+	}
+	response = OneSignal::Notification.create(params: params)
+	notification_id = JSON.parse(response.body)["id"]
+
     mail(to: 'roberto.vergallo@gmail.com',
          subject: subject,
          body: body+"<br/><br/>"+link,
          content_type: 'text/html')
 
-	unless player_id && player_id.empty?
-	    # notify the player (this will fail because we haven't configured the app yet)
-		params = {
-		  app_id: 'bf81fd5f-ada4-41ef-bd01-b44ef4cafd45',
-		  headings: subject,
-		  contents: body,
-		  ios_badgeType: 'None',
-		  ios_badgeCount: 1,
-		  include_player_ids: [player_id]
-		}
-		begin
-		  response = OneSignal::Notification.create(params: params)
-		  notification_id = JSON.parse(response.body)["id"]
-		rescue OneSignal::OneSignalError => e
-		  puts "--- OneSignalError  :"
-		  puts "-- message : #{e.message}"
-		  puts "-- status : #{e.http_status}"
-		  puts "-- body : #{e.http_body}"
-		end
-    end
   end
 end
