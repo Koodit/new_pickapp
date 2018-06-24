0bed0e5c0bc8bf194a40e38d33d7ecf372f3adbb
Roberto Vergallo <>
2018-06-24 17:01:56 +0200

    fixes

--- app/mailers/pick_app_mailer.rb
+++ app/mailers/pick_app_mailer.rb
@@ -5,27 +5,23 @@ class PickAppMailer < ApplicationMailer
          body: body+"<br/><br/>"+link,
          content_type: 'text/html')
 
-    puts "--- OneSignal : Entro?"
-	unless player_id && player_id.empty?
-		puts "--- OneSignal : SI"
-	    # notify the player (this will fail because we haven't configured the app yet)
-		params = {
-		  app_id: "bf81fd5f-ada4-41ef-bd01-b44ef4cafd45",
-		  headings: subject,
-		  contents: body,
-		  ios_badgeType: 'None',
-		  ios_badgeCount: 1,
-		  include_player_ids: [player_id]
-		}
-		begin
-		  response = OneSignal::Notification.create(params: params)
-		  notification_id = JSON.parse(response.body)["id"]
-		rescue OneSignal::OneSignalError => e
-		  puts "--- OneSignalError  :"
-		  puts "-- message : #{e.message}"
-		  puts "-- status : #{e.http_status}"
-		  puts "-- body : #{e.http_body}"
-		end
-    end
+    # notify the player (this will fail because we haven't configured the app yet)
+	params = {
+	  app_id: "bf81fd5f-ada4-41ef-bd01-b44ef4cafd45",
+	  headings: subject,
+	  contents: body,
+	  ios_badgeType: 'None',
+	  ios_badgeCount: 1,
+	  include_player_ids: [player_id]
+	}
+	begin
+	  response = OneSignal::Notification.create(params: params)
+	  notification_id = JSON.parse(response.body)["id"]
+	rescue OneSignal::OneSignalError => e
+	  puts "--- OneSignalError  :"
+	  puts "-- message : #{e.message}"
+	  puts "-- status : #{e.http_status}"
+	  puts "-- body : #{e.http_body}"
+	end
   end
 end
